fa6a6151afb93ee30d506dd9630dcd2818c2b1a2
Tvalc <tony@glittercloud.solutions>
2025-08-08 10:52:08 -0700

    Update from Snib Studio - 2025-08-08T17:52:01.485Z

---
+++ health_manager.js
@@ -0,0 +1,9 @@
+class HealthManager {
+    constructor(dragon) {
+        this.dragon = dragon;
+    }
+    update() {
+        // Handled in dragon_entity.js
+    }
+}
+window.HealthManager = HealthManager;
